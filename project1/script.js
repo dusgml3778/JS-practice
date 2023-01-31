590831aa23f95172f8fb302423cb95e73b19c606
kimmingye <dusgml3779@gmail.com>
2023-01-31 10:44:39 +0900

    Refactoring

--- project1/script.js
+++ project1/script.js
@@ -20,55 +20,75 @@ function showSuccess(input) {
 
 }
 
-function isValidEmail(email){
+function isValidEmail(email) {
 
-  if(email.value.indexOf('@')!= -1 ) {
+  if (email.value.indexOf('@') != -1) {
 
     let array = []
 
     array = email.value.split('@');
 
-      if(array[1].indexOf('.') != -1) {
-        
-        showSuccess(email);
-    
+    if (array[1].indexOf('.') != -1) {
+
+      if (array[1].substr(array[1].indexOf('.') + 1) === '') {
+        showError(email, "이메일 형식에 어긋납니다.")
       } else {
-    
-        showError(email, "이메일 형식에 어긋납니다.")   
+        showSuccess(email);
       }
 
+    } else {
+
+      showError(email, "이메일 형식에 어긋납니다.")
+    }
+
   } else {
-        showError(email, "이메일 형식에 어긋납니다.")
+    showError(email, "이메일 형식에 어긋납니다.")
 
   }
 }
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (item) {
+    if (item.value.trim() === '') {
+      console.log(item)
+      showError(item, `${getFieldName(item)} is required`)
+    } else {
+      showSuccess(item)
+    }
+  });
+
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if (input.value.length > max) {
+
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+
+  } else {
+    showSuccess(input)
+  }
+
+
+
+}
+
+function getFieldName(input) {
+
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+
+}
+
 
 // Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  if (username.value === '') {
-    showError(username, "UserName is required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === '') {
-    showError(email, "Email is required");
-  } else {
-    isValidEmail(email)
-  }
-
-  if (password.value === '') {
-    showError(password, "password is required");
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === '') {
-    showError(password2, "password2 is required");
-  } else {
-    showSuccess(password2);
-  }
-
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15)
+  checkLength(password, 6, 25)
+  isValidEmail(email)
 })
