1c95c4e0ab316552fb2dd52395dde78b4620d085
kimmingye <dusgml3779@gmail.com>
2023-03-09 14:05:08 +0900

    End of Dom Array

--- project4/script.js
+++ project4/script.js
@@ -33,14 +33,42 @@ async function getRandomUser() {
 }
 
 // Double everyones money
-function doubleMoney(){
+function doubleMoney() {
   data = data.map((user) => {
-    return {...user,money: user.money * 2}
+    return {
+      ...user,
+      money: user.money * 2
+    }
   });
 
   updateDom()
 }
 
+// Sort users by richest
+function sortByRichest() {
+
+  data.sort((a, b) => b.money - a.money)
+
+  updateDom();
+}
+
+// Filter only 1000000
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
+
+  updateDom()
+}
+
+// Calculate the total wealth
+function calculateWealth(){
+  const wealth = data.reduce((acc,user) => (acc +=user.money), 0);
+
+  const wealthEl = document.createElement('div')
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+
+  main.appendChild(wealthEl)
+
+}
 
 // Add new obj to data arr
 function addData(obj) {
@@ -52,13 +80,13 @@ function addData(obj) {
 }
 
 // Update Dom
-function updateDom(provideData = data){
+function updateDom(provideData = data) {
 
   //Clear main div
   main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
 
 
-  provideData.forEach(function(item){
+  provideData.forEach(function (item) {
 
     const el = document.createElement('div');
     el.classList.add('person')
@@ -69,10 +97,13 @@ function updateDom(provideData = data){
 }
 
 // Format number as money
-function formatMoney(number){
+function formatMoney(number) {
 
-  return '$'+number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
-addUserbtn.addEventListener('click',getRandomUser);
-doubleBtn.addEventListener('click',doubleMoney)
+addUserbtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires)
+calculateWealthBtn.addEventListener('click', calculateWealth)
