d7cca22fa5a2879ffdd42b4a3ec638e4989991bd
kimmingye <dusgml3779@gmail.com>
2023-03-30 16:50:09 +0900

    search function

--- project5/script.js
+++ project5/script.js
@@ -1,25 +1,39 @@
 const el = document.getElementById("main")
 const addBtn = document.getElementById("add-user")
+const doubleBtn = document.getElementById("double")
 const CLICKED_CLASS = "clicked";
 const close = document.getElementById("close");
 const open = document.getElementById("open");
 const modal = document.getElementById("modal");
 
+function info(value) {
 
-
-function info() {
-  fetch('https://jsonplaceholder.typicode.com/posts/')
+  fetch('https://jsonplaceholder.typicode.com/users/')
     .then(res => res.json())
     .then((data) => {
-      console.log(data)
-      for (let val = 0; val < data.length; val++) {
+      if (isEmpty(value)) {
+        for (let val = 0; val < data.length; val++) {
+          const els = document.createElement('div');
+          els.innerHTML = `<h4>${data[val].id} : ${data[val].name} : ${data[val].address.city}</h4>`
+          el.append(els)
+        }
+      } else if (!isEmpty(value)) {
         const els = document.createElement('div');
-        els.innerHTML = `<h4>${data[val].id} : ${data[val].title}</h4>`
+        els.innerHTML = `<h4>${data[value].id} : ${data[value].name} : ${data[value].address.city}</h4>`
         el.append(els)
+
       }
 
     })
 
+}
+
+function isEmpty(str) {
+
+  if (typeof str == "undefined" || str == null || str == "")
+    return true;
+  else
+    return false;
 }
 
 addBtn.addEventListener('click', function () {
@@ -38,10 +52,34 @@ addBtn.addEventListener('click', function () {
 
   }
 
-
 })
+
+doubleBtn.addEventListener('click', function () {
+
+  el.innerHTML = "";
+  const els = document.createElement('input')
+  els.setAttribute('type', 'text')
+  els.setAttribute('id', 'searchVal')
+  const button = document.createElement('button')
+  button.setAttribute('id', 'searchId')
+  button.innerHTML = '아이디 입력'
+  el.append(els);
+  el.append(button);
+
+  const searchVal = document.getElementById("searchVal")
+  const searchId = document.getElementById("searchId")
+
+  searchId.addEventListener('click', function () {
+
+    info(searchVal.value);
+
+  })
+})
+
+
 
 open.addEventListener("click", function () {
 
   modal.style.display = "flex"
+
 })
